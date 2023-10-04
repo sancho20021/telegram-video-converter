642df8ef1e854ba97b815c35676340732711c099
Aleksandr Pak <aleksandr.vl.pak@gmail.com>
2023-10-05 00:18:02 +0300

    fix incorrect output dimensions, fix bugs and refactor

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import sys
+from concurrent.futures import ThreadPoolExecutor
+from typing import Optional
 
 import click
 import mimetypes
@@ -6,7 +7,7 @@ import re
 import subprocess as sp
 
 from pathlib import Path
-from tqdm import tqdm
+from tempfile import NamedTemporaryFile
 
 
 class RecipeCreator:
@@ -36,7 +37,6 @@ class RecipeCreator:
         return self
 
     def resize(self, input_width: int, input_height: int):
-        print(f'width: {input_width}, height: {input_height}')
         if input_width > input_height:
             output_width = 512
             output_height = int((input_height * 512 / input_width) // 2 * 2)
@@ -46,7 +46,9 @@ class RecipeCreator:
         self.options.append(f'-filter:v scale="{output_width}:{output_height}"')
         return self
 
-    def set_output(self, output: str):
+    def set_output(self, output: str, file_format: Optional[str] = None):
+        if file_format:
+            self.options.append(f'-f {file_format}')
         self.options.append(output)
         return self
 
@@ -54,47 +56,62 @@ class RecipeCreator:
         return ' '.join(self.options)
 
 
-class Converter:
-    def __init__(self, ffmpeg: Path, ffprobe: Path, input_path: Path):
+class FFRunner:
+    def __init__(self, ffmpeg: Path, ffprobe: Path):
         self.ffmpeg = ffmpeg
         self.ffprobe = ffprobe
-        self.input_path = input_path
 
-    @staticmethod
-    def parse_resolution(s: str) -> (int, int):
-        print(s)
-        return [int(x) for x in re.findall(r'(\d+)x(\d+)', s)[0]]
-
-    def get_resolution(self):
-        command = (f"{self.ffprobe}"
-                   f" -v error -select_streams v:0 -show_entries stream=width,height -of csv=s=x:p=0"
-                   f" {self.input_path}")
+    def run_ffprobe(self, args: str) -> str:
+        command = f"{self.ffprobe} {args}"
         print(f'Command will be run:\n{command}')
         result = sp.run(command, shell=True, capture_output=True)
         if result.returncode != 0:
             raise Exception(f"ffprobe returned nonzero return code:\n{result.stderr.decode('utf-8')}")
         output = result.stdout.decode('utf-8')
+        return output
+
+    def run_ffmpeg(self, args: str):
+        command = f"{self.ffmpeg} {args}"
+        print(f'Command will be run:\n{command}')
+        result = sp.run(command, shell=True, capture_output=True)
+        if result.returncode != 0:
+            raise Exception(f"ffmpeg returned nonzero return code:\n{result.stderr.decode('utf-8')}")
+
+
+class Converter:
+    def __init__(self, ffmpeg: Path, ffprobe: Path):
+        self.runner = FFRunner(ffmpeg, ffprobe)
+
+    @staticmethod
+    def parse_resolution(s: str) -> (int, int):
+        return [int(x) for x in re.findall(r'(\d+)x(\d+)', s)[0]]
+
+    def get_resolution(self, video: Path) -> (int, int):
+        output = self.runner.run_ffprobe(
+            f"-v error -select_streams v:0 -show_entries stream=width,height -of csv=s=x:p=0 {video}"
+        )
         return self.parse_resolution(output)
 
-    def convert(self, output_path: Path):
-        width, height = self.get_resolution()
-        recipe = RecipeCreator(). \
-            yes(). \
-            set_input(str(self.input_path)). \
-            convert_to_vp9(). \
-            cut(). \
-            remove_audio(). \
-            resize(width, height). \
-            set_output(str(output_path)). \
-            get_recipe()
-        command = f"{self.ffmpeg} {recipe}"
-        print(f'Command will be run:\n{command}')
-        result = sp.run(command, shell=True)
-        if result.returncode == 0:
+    def convert(self, input_path: Path, output_path: Path):
+        with NamedTemporaryFile(mode='w+b') as temp_file:
+            recipe = RecipeCreator(). \
+                yes(). \
+                set_input(str(input_path)). \
+                cut(). \
+                remove_audio(). \
+                set_output(temp_file.name, 'mp4'). \
+                get_recipe()
+            self.runner.run_ffmpeg(recipe)
+            width, height = self.get_resolution(Path(temp_file.name))
+            recipe = RecipeCreator(). \
+                yes(). \
+                set_input(temp_file.name). \
+                convert_to_vp9(). \
+                resize(width, height). \
+                set_output(str(output_path)). \
+                get_recipe()
+            self.runner.run_ffmpeg(recipe)
             print(f'Success!!! File saved to {output_path}')
-        else:
-            print('Video conversion failed!!!', file=sys.stderr)
-            print(result.stderr)
 
 
 def generate_name(filename: str) -> str:
@@ -114,15 +131,23 @@ def convert_cli(ffmpeg: Path, ffprobe: Path, videos: Path):
     output_dir = videos.joinpath('converted')
     output_dir.mkdir(exist_ok=True)
 
-    for file in tqdm(videos.iterdir()):
+    inputs_outputs: list[(Path, Path)] = []
+    for file in videos.iterdir():
         if not file.is_file():
             continue
         if not is_video(file):
             print(f'{file} is not a video. Skipping')
             continue
-
         output_path = output_dir.joinpath(generate_name(file.name))
-        Converter(ffmpeg, ffprobe, file).convert(output_path)
+        inputs_outputs.append((file, output_path))
+
+    with ThreadPoolExecutor(max_workers=8) as executor:
+        results = executor.map(
+            lambda files: Converter(ffmpeg, ffprobe).convert(files[0], files[1]),
+            inputs_outputs
+        )
+        for _ in results:
+            pass
 
 
 if __name__ == '__main__':
